088ecaad72c5da8a103c0ec092f9152bc90fe00a
Psykisk <joakim.vanstrom@gmail.com>
2023-06-20 14:27:41 +0200

    moved out database and models to own projects

---
+++ Vanis/Controllers/CategoryController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vanis.DataAccess.Data;
+using Vanis.Models;
+
+namespace VanisBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) { 
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot match the name");
+            }
+            if (ModelState.IsValid)
+            {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();   
+            }
+            Category getCategory = _db.Categories.Find(id);
+            //Category getCategory2 = _db.Categories.FirstOrDefault(c=>c.Id == id);
+            //Category getCategory3 = _db.Categories.Where(c=>c.Id==id).FirstOrDefault();
+
+            if (getCategory == null)
+            {
+                return NotFound();
+            }
+            return View(getCategory);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category getCategory = _db.Categories.Find(id);
+
+            if (getCategory == null)
+            {
+                return NotFound();
+            }
+            return View(getCategory);
+        }
+        [HttpPost, ActionName("Delete")]
+
+        public IActionResult DeletePOST(int? id)
+        {
+            Category obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index", "Category");
+        }
+
+    }
+}
---
+++ VanisRazor_temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VanisRazor_temp.Data;
+using VanisRazor_temp.Models;
+
+namespace VanisRazor_temp.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+
+            return RedirectToPage("Index");
+        }
+    }
+}
